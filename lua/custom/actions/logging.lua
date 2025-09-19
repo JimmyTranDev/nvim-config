6672fa1539633608745bde3b56bb0e8d3533e6d5
Jimmy Tran <jimmytrandev@gmail.com>
2025-09-19 09:02:25 +0200

    fix: update logHistory function to use repository name instead of folder name

--- lua/custom/actions/logging.lua
+++ lua/custom/actions/logging.lua
@@ -1,13 +1,14 @@
 local inputUtils = require('custom.utils.input')
 local loggingUtils = require('custom.utils.logging')
+local githubUtils = require('custom.utils.github')
 
 local M = {}
 
-function M.logHistory(folderName, commitMessagePrefix, prompt)
+function M.logHistory(_, commitMessagePrefix, prompt)
   return function()
     local message = inputUtils.getInputFromUser(prompt .. ': ')
-
-    loggingUtils.logHistory(folderName, commitMessagePrefix .. message)
+    local repoName = githubUtils.getRepoName()
+    loggingUtils.logHistory(repoName, commitMessagePrefix .. message)
   end
 end
 
