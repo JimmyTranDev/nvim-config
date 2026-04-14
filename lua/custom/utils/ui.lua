8f80328bd847ac1605638e2710976a5f6cbd719c
Jimmy Tran <jimmytrandev@gmail.com>
2026-04-14 23:36:07 +0200

    style: 💎 apply stylua formatting to changed files

--- lua/custom/utils/ui.lua
+++ lua/custom/utils/ui.lua
@@ -36,9 +36,7 @@ M.show_success = function(msg) vim.notify(msg, vim.log.levels.INFO) end
 function M.exec_in_terminal(cmd, success_msg, terminal_id)
   if not cmd then error('Command is required') end
   vim.cmd(string.format("TermExec%d cmd='%s'", terminal_id or 5, cmd))
-  if success_msg then
-    vim.defer_fn(function() vim.notify(success_msg, vim.log.levels.INFO) end, 500)
-  end
+  if success_msg then vim.defer_fn(function() vim.notify(success_msg, vim.log.levels.INFO) end, 500) end
 end
 
 function M.multiline_input(opts, callback)
@@ -97,8 +95,6 @@ function M.multiline_input(opts, callback)
   vim.cmd('startinsert')
 end
 
-function M.add_back_option(options, text, value)
-  table.insert(options, { name = '← ' .. text, is_back = true, value = value or '__back__' })
-end
+function M.add_back_option(options, text, value) table.insert(options, { name = '← ' .. text, is_back = true, value = value or '__back__' }) end
 
 return M
--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -13,7 +13,5 @@ return {
   keys = {
     { '<leader>;D', '<cmd>DBUIToggle<CR>', desc = 'Database UI', mode = 'n' },
   },
-  init = function()
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
+  init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
 }
