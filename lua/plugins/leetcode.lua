512dc6102afe9d3e02390b3b41cd6b37864cbb43
Jimmy Tran <jimmytrandev@gmail.com>
2026-05-06 18:02:34 +0200

    feat(leetcode): add nerd font icons to all keymap descriptions, replace emoji

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -11,22 +11,22 @@ return {
     { '<leader><leader>ll', '<cmd>Leet<CR>', desc = '󰞷 Open LeetCode', mode = 'n' },
     { '<leader><leader>lm', '<cmd>Leet menu<CR>', desc = '󰍉 LeetCode menu', mode = 'n' },
     { '<leader><leader>lc', '<cmd>Leet console<CR>', desc = '󰆍 Open console', mode = 'n' },
-    { '<leader><leader>li', '<cmd>Leet info<CR>', desc = 'ℹ️  Problem info', mode = 'n' },
+    { '<leader><leader>li', '<cmd>Leet info<CR>', desc = '󰋽 Problem info', mode = 'n' },
 
-    { '<leader><leader>lL', '<cmd>Leet list<CR>', desc = '📋 List problems', mode = 'n' },
-    { '<leader><leader>ld', '<cmd>Leet daily<CR>', desc = '📅 Daily challenge', mode = 'n' },
-    { '<leader><leader>lR', '<cmd>Leet random<CR>', desc = '🎲 Random problem', mode = 'n' },
+    { '<leader><leader>lL', '<cmd>Leet list<CR>', desc = '󰉋 List problems', mode = 'n' },
+    { '<leader><leader>ld', '<cmd>Leet daily<CR>', desc = '󰦉 Daily challenge', mode = 'n' },
+    { '<leader><leader>lR', '<cmd>Leet random<CR>', desc = '󰛄 Random problem', mode = 'n' },
 
-    { '<leader><leader>lr', '<cmd>Leet run<CR>', desc = '▶️  Run code', mode = 'n' },
-    { '<leader><leader>lS', '<cmd>Leet submit<CR>', desc = '📤 Submit solution', mode = 'n' },
-    { '<leader><leader>lT', '<cmd>Leet test<CR>', desc = '🧪 Run tests', mode = 'n' },
+    { '<leader><leader>lr', '<cmd>Leet run<CR>', desc = '󰃒 Run code', mode = 'n' },
+    { '<leader><leader>lS', '<cmd>Leet submit<CR>', desc = '󰜐 Submit solution', mode = 'n' },
+    { '<leader><leader>lT', '<cmd>Leet test<CR>', desc = '󰙨 Run tests', mode = 'n' },
 
-    { '<leader><leader>ls', '<cmd>Leet session<CR>', desc = '🔗 Manage session', mode = 'n' },
-    { '<leader><leader>ly', '<cmd>Leet yank<CR>', desc = '📋 Yank solution', mode = 'n' },
+    { '<leader><leader>ls', '<cmd>Leet session<CR>', desc = '󰌐 Manage session', mode = 'n' },
+    { '<leader><leader>ly', '<cmd>Leet yank<CR>', desc = '󰈙 Yank solution', mode = 'n' },
 
-    { '<leader><leader>lg', '<cmd>Leet lang<CR>', desc = '🌐 Change language', mode = 'n' },
-    { '<leader><leader>lD', '<cmd>Leet desc<CR>', desc = '📖 Toggle description', mode = 'n' },
-    { '<leader><leader>lC', '<cmd>Leet cache<CR>', desc = '💾 Cache operations', mode = 'n' },
+    { '<leader><leader>lg', '<cmd>Leet lang<CR>', desc = '󰌐 Change language', mode = 'n' },
+    { '<leader><leader>lD', '<cmd>Leet desc<CR>', desc = '󰈙 Toggle description', mode = 'n' },
+    { '<leader><leader>lC', '<cmd>Leet cache<CR>', desc = '󰨮 Cache operations', mode = 'n' },
   },
   opts = {
     arg = 'leetcode.nvim',
@@ -82,9 +82,9 @@ return {
         local keymap = vim.keymap
         local function map(mode, lhs, rhs, desc) keymap.set(mode, lhs, rhs, { desc = desc, silent = true, noremap = true }) end
 
-        map('n', '<CR>', '<cmd>Leet run<CR>', 'Run code')
-        map('n', 's', '<cmd>Leet submit<CR>', 'Submit solution')
-        map('n', 't', '<cmd>Leet test<CR>', 'Run tests')
+        map('n', '<CR>', '<cmd>Leet run<CR>', '󰃒 Run code')
+        map('n', 's', '<cmd>Leet submit<CR>', '󰜐 Submit solution')
+        map('n', 't', '<cmd>Leet test<CR>', '󰙨 Run tests')
       end,
     })
   end,
