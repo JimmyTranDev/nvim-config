eba2a627b5e024a8b99dfea9cd34fe85372b500c
Jimmy Tran <jimmytrandev@gmail.com>
2026-05-06 18:04:26 +0200

    feat(dropbar): add breadcrumb icon to keymap descriptions

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -8,8 +8,8 @@ return {
   },
   config = function()
     local dropbar_api = require('dropbar.api')
-    vim.keymap.set('n', '<Leader><Leader>d', dropbar_api.pick, { desc = 'Dropbar' })
-    vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-    vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    vim.keymap.set('n', '<Leader><Leader>d', dropbar_api.pick, { desc = '󰍍 Dropbar' })
+    vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = '󰍍 Go to start of current context' })
+    vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = '󰍍 Select next context' })
   end,
 }
