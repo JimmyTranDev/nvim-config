d13baffcf3f411cc7f1308c39152b5f50ed6baee
Jimmy Tran <jimmytrandev@gmail.com>
2026-05-06 18:06:45 +0200

    feat(workspace-diagnostics): add diagnostic icon to keymap descriptions

--- lua/plugins/workspace-diagnostics.lua
+++ lua/plugins/workspace-diagnostics.lua
@@ -9,7 +9,7 @@ return {
           require('workspace-diagnostics').populate_workspace_diagnostics(client, 0)
         end
       end,
-      desc = 'Workspace Diagnostics',
+      desc = '󰒡 Workspace Diagnostics',
     },
   },
   config = function() require('workspace-diagnostics').setup() end,
