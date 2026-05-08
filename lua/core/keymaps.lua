23076f8a95a1e9f2438e6bb7c4f54885ea7fd100
Jimmy Tran <jimmytrandev@gmail.com>
2026-05-08 14:15:44 +0200

    feat(keymaps): add <Leader>uO binding for team PR filtering

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -163,6 +163,7 @@ map('n', '<Leader>ut', link_actions.open_technical_link, { desc = '󰦥 Open tec
 map('n', '<Leader>us', link_actions.search_google, { desc = '󰦥 Search Google' })
 map('v', '<Leader>us', link_actions.search_google, { desc = '󰦥 Search Google (selection)' })
 map('n', '<Leader>un', github_actions.show_notifications, { desc = '󰦥 GitHub Notifications' })
+map('n', '<Leader>uO', github_actions.select_open_prs_by_people, { desc = '󰦥 Open PRs by team' })
 
 -- New
 -- map('n', '<Leader>un', link_actions.open_npm_url, { desc = 'Open NPM link' })
